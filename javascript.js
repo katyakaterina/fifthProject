b9069f0519e20b7f6f3998eefab1e6de69bcdb85
katyakaterina <30529016+katyakaterina@users.noreply.github.com>
2017-10-11 20:13:57 +0300

    -

--- javascript.js
+++ javascript.js
@@ -1,11 +1,31 @@
  var button = document.querySelector(".write-us");
  var modal = document.querySelector(".modal");
  var mouseover = modal.querySelector(".close");
+ var login = modal.querySelector("[name=login]");
+ var email = modal.querySelector("[name=email]");
+ var form = modal.querySelector("form");
+ var submit = modal.querySelector(".submit");
+
           
- button.addEventListener("click", function () {
+ button.addEventListener("click", function (e) {
+ 	 e.preventDefault();
+ 	 console.log(modal.classList.contains("open"));
      modal.classList.toggle("open");
+     login.focus();
 
           });
+ form.addEventListener("submit", function(e) {
+ 	
+
+ 	if(!login.value || !email.value) {
+ 		console.log("пусто")
+ 	} 
+ 	
+ 	
+ });
+ 
+
+ 
   
         
           
