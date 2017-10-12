57997e7a8e4f20bcc268fcb0f1c48f79b71dea0e
katyakaterina <30529016+katyakaterina@users.noreply.github.com>
2017-10-12 13:22:49 +0300

    -

--- javascript.js
+++ javascript.js
@@ -3,32 +3,56 @@
  var mouseover = modal.querySelector(".close");
  var login = modal.querySelector("[name=login]");
  var email = modal.querySelector("[name=email]");
+ var letter = modal.querySelector("[name=letter]");
  var form = modal.querySelector("form");
  var submit = modal.querySelector(".submit");
+ var error = modal.querySelector("span");
+ var storage = localStorage.getItem("login");
 
           
  button.addEventListener("click", function (e) {
  	 e.preventDefault();
  	 console.log(modal.classList.contains("open"));
      modal.classList.toggle("open");
-     login.focus();
+
+
+
+     if(storage) {
+     	login.value = storage;
+     	email.focus();
+     } else {
+     	login.focus();
+     }
+
+
+     
 
           });
 
- form.addEventListener("submit", function(e) {
+ 
+ 	form.addEventListener("submit", function(e) {
  	e.preventDefault();
- 	
 
- 	if(!login.value) {
- 		console.log("пусто")
+ 	if(!login.value || !email.value) {
+ 		error.classList.add("err");
  	
  
  	 } else {
+ 	 	localStorage.setItem("Ваше имя",login.value);
  		console.log("не отправилась");
  	
  	}
- 	
- });
+    
+    });
+
+    
+
+
+
+
+   
+    
+   
  
 
  
